69e1af7aaf32f1c722c3d66c5f3216ac143f2179
YoonJungHae <yoongomtang@naver.com>
2024-08-02 11:24:15 +0900

    💦 chore :: (정해 #17)  관리자페이지 프론트 작업
    관리자페이지 메뉴바 생성, common/menubar.html에서 닉네임이랑 프로필 클릭시 admin권한으로 adminMain으로 갈 수 있게 설정

---
+++ src/main/java/com/multi/udong/admin/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.multi.udong.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/adminMain")
+    public String adminMain() {
+        return "admin/adminMain";
+    }
+}
