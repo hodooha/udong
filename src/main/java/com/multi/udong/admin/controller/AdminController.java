4db0ba402b411d967de209811b71f4ddce248cc9
YoonJungHae <yoongomtang@naver.com>
2024-08-03 23:42:12 +0900

    🧩feat :: (정해 #19) 회원관리탭 검색기능, 페이지네이션 추가
    회원ID, 닉네임으로 검색 가능, 한 페이지당 회원6명씩 조회 가능

--- src/main/java/com/multi/udong/admin/controller/AdminController.java
+++ src/main/java/com/multi/udong/admin/controller/AdminController.java
@@ -1,12 +1,16 @@
 package com.multi.udong.admin.controller;
 
 import com.multi.udong.admin.service.AdminService;
+import org.apache.ibatis.javassist.compiler.ast.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -23,6 +27,17 @@ public class AdminController {
     @GetMapping("/adminMain")
     public String adminMain(Model model) {
         model.addAttribute("members", adminService.getAllMembers());
+        return "admin/adminMain";
+    }
+    @GetMapping("/memberSearch")
+    public String searchMembers(@RequestParam(value = "search", required = false) String search, Model model) {
+        if (search == null || search.trim().isEmpty()) {
+            // 검색어가 없을 경우 기본 화면으로 리디렉션
+            return "redirect:/admin/adminMain";
+        }
+
+        List<Member> members = adminService.searchMembersByIdOrNickname(search);
+        model.addAttribute("members", members);
         return "admin/adminMain";
     }
 }
--- src/main/java/com/multi/udong/admin/model/dao/AdminMapper.java
+++ src/main/java/com/multi/udong/admin/model/dao/AdminMapper.java
@@ -2,10 +2,14 @@ package com.multi.udong.admin.model.dao;
 
 import com.multi.udong.member.model.dto.MemberDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.javassist.compiler.ast.Member;
 
 import java.util.List;
 
 @Mapper
 public interface AdminMapper {
     List<MemberDTO> getAllMembers();
+    List<Member> searchMember(@Param("memberId") String memberId, @Param("nickname") String nickname);
+
 }
